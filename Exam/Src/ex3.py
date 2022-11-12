0f495d59652f4c7c6ae00f04feb8f622a87de94a
Willi Kappler <willi.kappler.gm@gmail.com>
2022-11-12 21:32:44 +0100

    Finish exam 3, fix typo for exam 0

---
+++ Exam/Src/ex3.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def filter_data(d):
+    result = []
+    values = []
+    for i in range(len(d)):
+        if d[i] > -9000.0:
+            values.append(d[i])
+            if len(values) == 24:
+                result.append(sum(values) / 24.0)
+                values = []
+    return result
+
+# Santa Gracia, 2017
+data = np.loadtxt("data3.csv", delimiter=",", usecols=(1,2))
+
+# Air temperature
+result1 = filter_data(data[:, 0])
+
+# Air relative humidity
+result2 = filter_data(data[:, 1])
+
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(result1)
+ax[1].plot(result2)
+plt.show()
